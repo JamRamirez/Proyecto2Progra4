781e60720e23aa576ef4f7319d24e79fdbf39ef5
estcast <84981457+estcast@users.noreply.github.com>
2021-06-09 03:52:12 +0200

    Los asientos en rojo están ocupados por eso no hacen nada. Son solo ejemplo para ver esa funcionalidad. Ahora el popUp enlista las butacas seleccionadas y calcula un precio. Ya luego con el precio de la pelìcula se arregla.

--- web/js/script.js
+++ web/js/script.js
@@ -1,6 +1,7 @@
 
 var peliculas = [{nombre: "Totoro"}, {nombre: "JohnWick"},{nombre: "Fast&Furious6"},{nombre: "JurassicPark"},{nombre: "Jaws"}];
-
+var asientos = [];
+var precioPelicula = 1000;
 
 function render() {
 
@@ -39,28 +40,50 @@ function loaded(event){
     addEventSeats();
 }
 
-function displayPop(){
+function displayPop(){                                        //muestra el popUp
     document.getElementById("over").className = "overlay";
     document.getElementById("pop").style.display='block';
 }
 
-function hidePop(){
+function hidePop(){                                                   //oculta el popUP
     document.getElementById("over").classList.remove("overlay");
     document.getElementById("pop").style.display='none';
 }
 
-function colorChanger(e){
-    if(!e.classList.contains("occupied")){ 
-        e.classList.add("occupied");
+function colorChanger(e){                               //Evento que cambia de color los asientos
+    if(e.classList.contains("occupied"))
+        return;
+    
+    if(!e.classList.contains("selected")){ 
+        e.classList.add("selected");
+        asientos.push(e.getAttribute("id"));
+        updateInfo();
     } 
-    else e.classList.remove("occupied");
+    else {
+        e.classList.remove("selected");
+        asientos.splice(asientos.indexOf(e.getAttribute("id")),1);    //Elimina el asiento del array
+        updateInfo();
+    }
 }
 
-function addEventSeats(){
+function addEventSeats(){                                   //Agrega el evento a cada asiento                           
     container = document.querySelectorAll(".seat");
     container.forEach(box =>
         box.addEventListener("click",(b)=>{colorChanger(box);})
     );
+}
+
+function updateInfo(){
+    datos = document.getElementById("datos");
+    datos.textContent = "Asientos seleccionados: " + asientos.length.toString();
+    
+    div = document.createElement("div");
+    div.textContent += "Butacas: " + asientos;
+    datos.appendChild(div);
+    
+    div2 = document.createElement("div");
+    div2.textContent = "Total: " + (asientos.length * precioPelicula).toString() + " colones";
+    datos.appendChild(div2);
 }
 
 document.addEventListener("DOMContentLoaded", loaded);
